206cdb173d151c0b2ee14d575cdfb83d48c27878
mauricci <MaurizioRicci@users.noreply.github.com>
2019-10-21 13:22:39 +0200

    login rewrite with util functions

--- login.php
+++ login.php
@@ -1,37 +1,26 @@
 <?php
+
 include('connection.php');
-http_response_code(401);
-$res = array(
-	"ok" => false,
-	"role" => '',
-	"msg" => 'Username/Password invalid'
-);
-	
-if (isset($_POST['username']) && isset($_POST['password']) ) {
-	$username = $_POST['username'];
-	$password = $_POST['password'];
-	
-	$result = pg_prepare($conn,'', "SELECT * FROM utenti WHERE username=$1 and password=$2");
-	if($result){
-		$result = pg_execute($conn,'', array($username, $password));
-		if (!$result) {
-		  http_response_code(500);
-		  echo "An error occurred.\n";
-		  exit;
-		}
-		
-		if ($row = pg_fetch_assoc($result)) {
-		  if ($row['username'] == $username) {
-			if ($row['password'] == $password){
-				$res["ok"] = true;
-				$res["role"] = $row['role'];
-				$res["msg"] = '';
-				http_response_code(200);
-			}
-		  }
-		}
-	}
-}
+include('utils.php');
+include('queryUtils.php');
 header('Content-type: application/json');
+$c = 0;
+http_response_code(500);
+$My_POST = postEmptyStr2NULL();
+$res = array(
+    "role" => '',
+    "msg" => ''
+);
+
+$user = risolviUtente($conn, $c++, $My_POST['username'], $My_POST['password']);
+if (!isset($user)) {
+    http_response_code(401);
+    $res['msg'] = 'Username/Password invalidi';
+    $error = true;
+}
+else {
+    http_response_code(200);
+    $res['role'] = $user['role'];
+}
+
 echo json_encode($res);
-?>
