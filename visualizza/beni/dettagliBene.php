16de1f88ef2abb0260820f6daa243f52b9c47f5b
mauricci <MaurizioRicci@users.noreply.github.com>
2019-10-22 10:18:30 +0200

    dettagli bene db definitivo o temporaneo

--- visualizza/beni/dettagliBene.php
+++ visualizza/beni/dettagliBene.php
@@ -1,28 +1,30 @@
 <?php
+
 include('../../connection.php');
 include('../../utils.php');
 
 header('Content-type: application/json');
 $res = array();
 http_response_code(500);
-	
-if (isset($_GET['id']) ) {
-	
-	$id = $_GET['id'];
-	$query = "SELECT *, ST_AsGeoJSON(geom) as geojson, ST_AsGeoJSON(ST_Centroid(geom)) " .
-	"as centroid_geojson FROM benigeo_e_schedatori WHERE id=$1";
-	$result = pg_prepare($conn,'', $query);
-	if($result){
-		$result = pg_execute($conn,'', array($id));
-		if ($result) {		
-			while ($row = pg_fetch_assoc($result)) {
-			  $res = beniPostgres2JS($row);
-			}
-			http_response_code(200);
-		}
-		else
-			$res['msg'] = pg_result_error ($conn);
-	}
+
+if (isset($_GET['id'])) {
+
+    $id = $_GET['id'];
+    $table = filter_input(INPUT_GET, 'tmp_db', FILTER_VALIDATE_BOOLEAN) ?
+            'tmp_db.benigeo' : 'benigeo_e_schedatori';
+    $query = "SELECT *, ST_AsGeoJSON(geom) as geojson, ST_AsGeoJSON(ST_Centroid(geom)) " .
+            "as centroid_geojson FROM $table WHERE id=$1";
+    $result = pg_prepare($conn, '', $query);
+    if ($result) {
+        $result = pg_execute($conn, '', array($id));
+        if ($result) {
+            while ($row = pg_fetch_assoc($result)) {
+                $res = beniPostgres2JS($row);
+            }
+            http_response_code(200);
+        } else
+            $res['msg'] = pg_result_error($conn);
+    }
 }
 echo json_encode($res);
 ?>
