2ef798d8e35f9351eb4a4d71f9d9e4d83bf6ed02
mauricci <MaurizioRicci@users.noreply.github.com>
2019-11-04 10:10:50 +0100

    aggiunta query per tutti i beni aggiunti da chiunque

--- visualizza/beni/beniAggiuntiRevisione.php
+++ visualizza/beni/beniAggiuntiRevisione.php
@@ -18,8 +18,8 @@ if (!isset($utente) && !$error) {
     $res['msg'] = 'Username/Password invalidi';
     $error = true;
 }
-// Ottengo in beni inseriti da un certo utente oppure i suoi beni in revisione
-$query_beni_aggiunti = 'SELECT 
+// Ottengo in beni inseriti da un certo utente
+$query_beni_aggiunti_miei = 'SELECT 
         b.id,
         b.ident,
         b.descr,
@@ -36,7 +36,25 @@ $query_beni_aggiunti = 'SELECT
     FROM benigeo as b inner join manipola_bene as m ON(b.id=m.id_bene)
     WHERE m.id_utente=$1';
 
-$query_beni_revisione = 'SELECT 
+// Ottengo tutti i beni inseriti
+$query_beni_aggiunti_tutti = 'SELECT 
+        b.id,
+        b.ident,
+        b.descr,
+        b.meo,
+        b.mec,
+        b.topon,
+        b.esist,
+        b.comun,
+        b.bibli,
+        b.note,
+        b.geom,
+        m.id_utente,
+        m.timestamp_utc
+    FROM benigeo_e_schedatori';
+
+// Ottengo in beni in revisione di un certo utente
+$query_beni_revisione_miei = 'SELECT 
         b.id,
         b.ident,
         b.descr,
@@ -52,7 +70,7 @@ $query_beni_revisione = 'SELECT
         b.timestamp_utc,
         b.status,
         b.msg_validatore
-    FROM tmp_db.benigeo as b WHERE b.id_utente=$1 AND (b.status BETWEEN 0 AND 1)';
+    FROM tmp_db.benigeo as b WHERE (b.status BETWEEN 0 AND 1) AND b.id_utente=$1';
 // status = 0/1 per bene in attesa revisione/da rivedere
 
 if (!$error) {
@@ -61,24 +79,36 @@ if (!$error) {
     if ($utente['role'] == 'master') {
         // i revisori nel caso vogliano i beni in revisione devono averli tutti
         $params = array();
-        // vado quindi a rimuovere la parte finale della query '...WHERE id_utente=x'
+        // vado quindi a rimuovere la parte finale della query '...AND id_utente=x'
         // senza più il filtro i revisori vedono tutti i beni in revisione
-        $index = strripos($query_beni_revisione, 'WHERE');
+        $index = strripos($query_beni_revisione_miei, 'AND');
         $query_beni_revisione = substr($query_beni_revisione, 0, $index - 1);
     }
 
-    $query = isset($My_POST['switch_bene']) &&
-            $My_POST['switch_bene'] == 'aggiunti' ? $query_beni_aggiunti : $query_beni_revisione;
-    $resp = runPreparedQuery($conn, $c++, $query, $params);
-
-    if ($resp['ok']) {
-        // $res['echo'] = intval($My_POST['echo']);
-        $res['filtered'] = pg_num_rows($resp['data']);
-        $res['data'] = [];
-        while ($row = pg_fetch_assoc($resp['data'])) {
-            array_push($res['data'], beniPostgres2JS($row));
+    if (isset($My_POST['switch_bene'])) {
+        switch ($My_POST['switch_bene']) {
+            case 'miei_aggiunti':
+                $query = $query_beni_aggiunti_miei;
+                break;
+            case 'miei_revisione':
+                $query = $query_beni_revisione_miei;
+                break;
+            case 'tutti_aggiunti':
+                $query = $query_beni_aggiunti_tutti;
+                break;
         }
-        http_response_code(200);
+        //$query = $My_POST['switch_bene'] == 'aggiunti' ? $query_beni_aggiunti : $query_beni_revisione;
+        $resp = runPreparedQuery($conn, $c++, $query, $params);
+
+        if ($resp['ok']) {
+            // $res['echo'] = intval($My_POST['echo']);
+            $res['filtered'] = pg_num_rows($resp['data']);
+            $res['data'] = [];
+            while ($row = pg_fetch_assoc($resp['data'])) {
+                array_push($res['data'], beniPostgres2JS($row));
+            }
+            http_response_code(200);
+        }
     }
 }
 
