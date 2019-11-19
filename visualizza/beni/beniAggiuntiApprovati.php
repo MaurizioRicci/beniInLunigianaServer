a98c1ac53073713dd7c891eb8def2baa360965e0
mauricci <MaurizioRicci@users.noreply.github.com>
2019-11-19 11:31:40 +0100

    sorting

--- visualizza/beni/beniAggiuntiApprovati.php
+++ visualizza/beni/beniAggiuntiApprovati.php
@@ -14,6 +14,34 @@ http_response_code(200);
 // contenere troppi dati per caricarli tutti in memoria in un browser
 $limit = filter_input(INPUT_GET, 'limit', FILTER_SANITIZE_NUMBER_INT);
 $page = filter_input(INPUT_GET, 'page', FILTER_SANITIZE_NUMBER_INT);
+$sortDirection = filter_input(INPUT_GET, 'ascending', FILTER_SANITIZE_NUMBER_INT) == 1 ? 'ASC' : 'DESC';
+$columnToOrder = filter_input(INPUT_GET, 'orderBy', FILTER_SANITIZE_STRING);
+switch ($columnToOrder) {
+    case "id":
+        $columnToOrder = "id";
+        break;
+    case "identificazione":
+        $columnToOrder = "ident";
+        break;
+    case "descrizione":
+        $columnToOrder = "descr";
+        break;
+    case "comune":
+        $columnToOrder = "comun";
+        break;
+    case "macroEpocaCar":
+        $columnToOrder = "mec";
+        break;
+    case "macroEpocaOrig":
+        $columnToOrder = "meo";
+        break;
+    case "toponimo":
+        $columnToOrder = "topon";
+        break;
+    default:
+        $columnToOrder = "";
+        break;
+};
 $offset = $limit * ($page - 1);
 
 // imposto i filtri sui campi
@@ -82,16 +110,24 @@ if ($query_beni_aggiunti_tutti_where !== '') {
     // WHERE campo1 ilike 'xx' and campo2 ilike 'yyy' and true
     // true è un escamotage per far quadrare l'ultimo AND aggiunto
     $query_beni_aggiunti_tutti_where = ' WHERE ' . $query_beni_aggiunti_tutti_where . 'TRUE ';
+    // aggiungo il where alla select
+    $query_beni_aggiunti_tutti_select = $query_beni_aggiunti_tutti_select
+            . $query_beni_aggiunti_tutti_where;
 }
-$query_beni_aggiunti_tutti = $query_beni_aggiunti_tutti_select
-        . $query_beni_aggiunti_tutti_where
+
+if (isset($columnToOrder) && $columnToOrder !== '') {
+    $query_beni_aggiunti_tutti_select = $query_beni_aggiunti_tutti_select
+            . "ORDER BY $columnToOrder $sortDirection";
+}
+
+$query_beni_aggiunti_tutti_select = $query_beni_aggiunti_tutti_select
         . " LIMIT $1"
         . " OFFSET $2";
 
 $params = [$limit, $offset];
 
 // eseguo la query
-$query = runPreparedQuery($conn, $c++, $query_beni_aggiunti_tutti, $params);
+$query = runPreparedQuery($conn, $c++, $query_beni_aggiunti_tutti_select, $params);
 if ($query['ok']) {
     while ($row = pg_fetch_assoc($query['data'])) {
         array_push($res['data'], beniPostgres2JS($row));
