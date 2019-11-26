e33f1147590cd63c8504b0f7ea1324db58d15e63
mauricci <MaurizioRicci@users.noreply.github.com>
2019-11-26 21:22:21 +0100

    vedi bene in revisione se proprietario o revisore

--- visualizza/beni/dettagliBene.php
+++ visualizza/beni/dettagliBene.php
@@ -1,30 +1,55 @@
 <?php
 
 include('../../connection.php');
+include('../../queryUtils.php');
 include('../../utils.php');
 
 header('Content-type: application/json');
+// analizza $_POST e converte le stringhe vuote in null
+$My_POST = postEmptyStr2NULL();
 $res = array();
+$c = 0;
 http_response_code(500);
 
-if (isset($_GET['id'])) {
+if (isset($My_POST['id'])) {
 
-    $id = $_GET['id'];
-    $table = filter_input(INPUT_GET, 'tmp_db', FILTER_VALIDATE_BOOLEAN) ?
-            'tmp_db.benigeo' : 'benigeo_e_schedatori';
+    $id = $My_POST['id'];
     $query = "SELECT *, ST_AsGeoJSON(geom) as geojson, ST_AsGeoJSON(ST_Centroid(geom)) " .
-            "as centroid_geojson FROM $table WHERE id=$1";
-    $result = pg_prepare($conn, '', $query);
-    if ($result) {
-        $result = pg_execute($conn, '', array($id));
-        if ($result) {
-            while ($row = pg_fetch_assoc($result)) {
-                $res = beniPostgres2JS($row);
+            "as centroid_geojson FROM benigeo_e_schedatori WHERE id=$1";
+    $params = [$id];
+
+    // se tmpdb è settato ed è true allora ho bisogno anche di id_utente, username e password
+    if (isset($My_POST['tmp_db'])) {
+        $tmp_db = filter_var($My_POST['tmp_db'], FILTER_VALIDATE_BOOLEAN);
+        if ($tmp_db) {
+            $id_utente = $My_POST['id_utente'];
+            // controllo che se viene richiesto un bene in revisione, chi lo richiede sia o un revisore o il proprietario
+            if (isset($My_POST['username']) && isset($My_POST['id_utente'])) {
+                $utente = risolviUtente($conn, $c++, $My_POST['username'], $My_POST['password']);
+
+                if (($utente['id'] != $My_POST['id_utente']) &&
+                        ($utente['role'] !== 'revisore')) {
+                    http_response_code(422);
+                    $res['msg'] = 'Sei uno scedatore, non puoi vedere i beni in revisione altrui';
+                    echo json_encode($res);
+                    return;
+                }
             }
-            http_response_code(200);
-        } else
-            $res['msg'] = pg_result_error($conn);
+        }
+        // se cerco nel db temporaneo serve anche l'id utente
+        $query = "SELECT *, ST_AsGeoJSON(geom) as geojson, ST_AsGeoJSON(ST_Centroid(geom)) " .
+                "as centroid_geojson FROM tmp_db.benigeo WHERE id=$1 AND id_utente=$2";
+        $params = [$id, $id_utente];
+    }
+
+    $result = runPreparedQuery($conn, $c++, $query, $params);
+    if ($result['ok']) {
+        while ($row = pg_fetch_assoc($result['data'])) {
+            $res = beniPostgres2JS($row);
+        }
+        http_response_code(200);
+    } else {
+        $res['msg'] = pg_result_error($conn);
     }
 }
 echo json_encode($res);
-?>
