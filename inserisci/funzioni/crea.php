45ced0eca4eef8058ab443cc3e6d9acbee4328b3
mauricci <MaurizioRicci@users.noreply.github.com>
2019-12-09 11:38:52 +0100

    creazione funzione

---
+++ inserisci/funzioni/crea.php
@@ -0,0 +1,105 @@
+<?php
+
+include('../../connection.php');
+include('../../utils.php');
+include('../../queryUtils.php');
+
+header('Content-type: application/json');
+$res = array();
+$c = 0; // do un id progressivo alle query
+$error = false;
+http_response_code(500);
+$My_POST = dictEmptyStr2NULL(funzioniJS2Postgres($_POST));
+
+$user = risolviUtente($conn, $c++, $My_POST['username'], $My_POST['password']);
+if (!isset($user) && !$error) {
+    http_response_code(401);
+    $res['msg'] = 'Username/Password invalidi';
+    $error = true;
+}
+
+if (!$error) {
+    pg_query('BEGIN TRANSACTION ISOLATION LEVEL REPEATABLE READ') or die('Cant start transaction');
+    $resp1 = $resp2 = $resp3 = $queryID = null;
+
+    //in base al ruolo utente scelgo in quale tabella mettere il bene
+    if ($user['role'] == 'revisore') {
+
+        //senza revisione
+        // controllo che il bene non esista già
+        $queryID = runPreparedQuery($conn, $c++,
+                'SELECT id from funzionigeo where id=$1', [$My_POST['id']]);
+        if (pg_num_rows($queryID['data']) > 0) {
+            //richiesta sintatticamente corretta ma semanticamente errata
+            http_response_code(422);
+            $res['msg'] = "La funzione con id ${My_POST['id']} esiste già";
+            $error = true;
+        } else {
+            if (!esisteBene($conn, $c++, $My_POST['id_bene'], null)) {
+                http_response_code(422);
+                $error = true;
+                $res['msg'] = "Il bene ${My_POST['id_bene']} non esiste";
+            }
+            if (!esisteBene($conn, $c++, $My_POST['id_bener'], null)) {
+                http_response_code(422);
+                $error = true;
+                $res['msg'] = "Il bene ${My_POST['id_bener']} non esiste";
+            }
+            if (!$error) {
+                $resp1 = insertIntoFunzioniGeo($conn, $c++, $My_POST['id_bene'], $My_POST['id_bener'],
+                        $My_POST['denominazione'], $My_POST['denominazioner'], $My_POST['data'],
+                        $My_POST['tipodata'], $My_POST['funzione'],
+                        $My_POST['bibliografia'], $My_POST['note'], $user['id'], $My_POST['status']);
+                //manipolabene serve se è validato il bene
+                $id_funzione = getIdFunzione($resp1);
+                if (isset($id_funzione)) {
+                    $resp2 = insertIntoManipolaFunzione($conn, $c++, $user['id'], $id_funzione);
+                    $resp3 = insertFunzioniGeoRuoli($conn, $c++, $id_funzione, $My_POST['ruolo'],
+                            $My_POST['ruolor'], false);
+                    $error = $error || !$resp1['ok'] || !$resp2['ok'] || !$resp3['ok'];
+                }
+            }
+        }
+    } else if ($user['role'] == 'schedatore') {
+        if (!esisteBene($conn, $c++, $My_POST['id_bene'], $My_POST['id_utente_bene'])) {
+            http_response_code(422);
+            $error = true;
+            $res['msg'] = "Il bene ${My_POST['id_bene']} non esiste";
+        }
+        if (!esisteBene($conn, $c++, $My_POST['id_bener'], $My_POST['id_utente_bener'])) {
+            http_response_code(422);
+            $error = true;
+            $res['msg'] = "Il bene ${My_POST['id_bener']} non esiste";
+        }
+        if (!$error) {
+            $resp1 = insertIntoFunzioniGeoTmp($conn, $c++, $My_POST['id_bene'], $My_POST['id_bener'],
+                    $My_POST['denominazione'], $My_POST['denominazioner'], $My_POST['data'],
+                    $My_POST['tipodata'], $My_POST['funzione'], $My_POST['bibliografia'],
+                    $My_POST['note'], $user['id'], $My_POST['id_utente_bene'],
+                    $My_POST['id_utente_bener'], $My_POST['status']);
+            $id_funzione = getIdFunzione($resp1);
+            if (isset($id_funzione)) {
+                $resp2 = insertFunzioniGeoRuoli($conn, $c++, $id_funzione, $My_POST['ruolo'],
+                        $My_POST['ruolor'], true);
+            }
+        }
+    }
+
+    $queryArr = array($resp1, $queryID, $resp2, $resp3);
+    if (!$error && checkAllPreparedQuery($queryArr)) {
+        if (pg_query('COMMIT')) {
+            http_response_code(200);
+        } else {
+            $res['msg'] = $transazione_fallita_msg;
+        }
+    } else {
+        echo "rollback";
+        pg_query('ROLLBACK');
+        $failed_query = getFirstFailedQuery($queryArr);
+        if (!isset($res['msg']) && isset($failed_query)) { //magari ho già scritto io un messaggio d'errore
+            $res['msg'] = pg_result_error($failed_query['data']);
+        }
+    }
+}
+
+echo json_encode($res);
