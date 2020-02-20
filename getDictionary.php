86a64a98ee0a7352ecfe86c1af768ad8731bed27
mauricci <MaurizioRicci@users.noreply.github.com>
2020-02-20 14:39:16 +0100

    supporto un testo personalizzato per i dizionari

--- getDictionary.php
+++ getDictionary.php
@@ -1,29 +1,32 @@
 <?php
+
 include('connection.php');
 
 $res = array();
 http_response_code(500);
 
-if (isset($_GET['dict_name']) ) {
-	
-	$dict_name = $_GET['dict_name'];
-	$dict_name_esc = pg_escape_identifier($dict_name);
-	$result = pg_prepare($conn,'', "SELECT DISTINCT nome, row_number FROM vocabolari.$dict_name_esc ORDER BY row_number ASC");
-	if($result){
-		$result = pg_execute($conn,'', []);
-		if (!$result) {
-		  echo "An error occurred.\n";
-		  exit;
-		}
-		
-		while ($row = pg_fetch_row($result)) {
-			$temp = array(
-			  'id' => $row[0],
-			  'value' => $row[0]);
-		  array_push($res, $temp);
-		}
-		http_response_code(200);
-	}
+if (isset($_GET['dict_name'])) {
+
+    $dict_name = $_GET['dict_name'];
+    $dict_name_esc = pg_escape_identifier($dict_name);
+    $result = pg_prepare($conn, '', "SELECT * FROM vocabolari.$dict_name_esc ORDER BY row_number ASC");
+    if ($result) {
+        $result = pg_execute($conn, '', []);
+        if (!$result) {
+            echo "An error occurred.\n";
+            exit;
+        }
+
+        while ($row = pg_fetch_assoc($result)) {
+            $temp = [
+                'id' => $row['nome'],
+                'value' => $row['nome'],
+                'text' => array_key_exists('text', $row) && isset($row['text']) ? $row['text'] : $row['nome']
+            ];
+            array_push($res, $temp);
+        }
+        http_response_code(200);
+    }
 }
 header('Content-type: application/json');
 echo json_encode($res);
