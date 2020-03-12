c8c92775229f782cad9637c58032818c7406f832
mauricci <MaurizioRicci@users.noreply.github.com>
2020-03-12 18:11:33 +0100

    mi preparo a riprovare la transazione nel caso fallisca

--- valida/beni/valida.php
+++ valida/beni/valida.php
@@ -31,70 +31,75 @@ if (!isset($user) && !$error) {
  * 6 le funzioni dell'utente che puntavano a un bene temporaneo devono ora puntare a uno definitivo
  */
 if (isset($My_POST['id']) && !$error) {
-    // occorre proteggersi dalle possibili write skew risultanti 
-    // dalla modifica/creazione concorrente dello stesso bene da validare.
-    pg_query('BEGIN TRANSACTION ISOLATION LEVEL SERIALIZABLE') or die('Cant start transaction');
-    $resp0 = $resp1 = $resp2 = $resp3 = $resp4 = $resp5 = null;
+    // al massimo faccio il loop due volte nel caso la transazione fallisca
+    for ($tCount = 0; $tCount < 2; $tCount++) {
+        // occorre proteggersi dalle possibili write skew risultanti 
+        // dalla modifica/creazione concorrente dello stesso bene da validare.
+        pg_query('BEGIN TRANSACTION ISOLATION LEVEL SERIALIZABLE') or die('Cant start transaction');
+        $resp0 = $resp1 = $resp2 = $resp3 = $resp4 = $resp5 = null;
 
-    // PASSO 1. controllo il ruolo.
-    if ($user['role'] == 'revisore') {
-        //PASSO 2
-        $respBeneTmp = runPreparedQuery($conn, $c++,
-                'SELECT id from tmp_db.benigeo where id=$1 and id_utente=$2 FOR UPDATE', [$My_POST['id'], $My_POST['id_utente']]);
-        if (!$respBeneTmp['ok'] || pg_num_rows($respBeneTmp['data']) <= 0) {
-            $res['msg'] = 'ID del bene in revisione non trovato. Forse altri revisori hanno approvato il bene.';
-            $error = true;
-        }
-        if (isset($My_POST['id_utente'])) {
-            // se viene fornito anche id_utente allora è parte della chiave per un bene in archivio temporaneo
-            // copio il bene temporaneo in archivio definitivo e cancello il bene temporaneo
-            // PASSO 3
-            $resp0 = replaceIntoBeniGeoTmp($conn, $c++, $My_POST['id'], $My_POST['ident'],
-                    $My_POST['descr'], $My_POST['mec'], $My_POST['meo'], $My_POST['bibl'],
-                    $My_POST['note'], $My_POST['topon'], $My_POST['comun'], $My_POST['geom'],
-                    $My_POST['id_utente'], $My_POST['status'], $My_POST['esist']);
-            $resp1 = upsertBeneTmpToBeniGeo($conn, $c++, $My_POST['id'], $My_POST['id_utente']);
-            // segno chi ha fatto cosa. PASSO 4
-            $resp2 = insertIntoManipolaBene($conn, $c++, $My_POST['id_utente'], $My_POST['id']);
-            $error = $error || !$resp0['ok'] || !$resp1['ok'] || !$resp2['ok'];
-            // PASSO 5
-            $resp3 = runPreparedQuery($conn, $c++,
-                    'DELETE FROM tmp_db.benigeo WHERE id=$1 AND id_utente=$2',
-                    [$My_POST['id'], $My_POST['id_utente']]);
-            // PASSO 6
-            $resp4 = runPreparedQuery($conn, $c++,
-                    'UPDATE FROM tmp_db.funzionigeo WHERE id_bene=$1 AND id_utente_bene=$2 SET id_utente_bene=NULL',
-                    [$My_POST['id'], $My_POST['id_utente']]);
-            $resp5 = runPreparedQuery($conn, $c++,
-                    'UPDATE FROM tmp_db.funzionigeo WHERE id_bener=$1 AND id_utente_bener=$2 SET id_utente_bener=NULL',
-                    [$My_POST['id'], $My_POST['id_utente']]);
-        }
-    } else {
-        $error = true;
-        http_response_code(401);
-        $res['msg'] = 'Operazione non permessa. Non sei un revisore';
-    }
-
-    // per sicurezza controllo tutte le query
-    $queryArr = array($resp0, $resp1, $resp2, $resp3, $resp4, $resp5);
-    if (!$error && checkAllPreparedQuery($queryArr)) {
-        // se COMMIT è andato a buon fine
-        if (pg_query('COMMIT')) {
-            http_response_code(200);
-            logTxt($conn, "Valida bene", "ID utente: ${user['id']}, "
-                    . "ID bene: ${My_POST['id']}, ID utente bene: ${My_POST['id_utente']}");
+        // PASSO 1. controllo il ruolo.
+        if ($user['role'] == 'revisore') {
+            //PASSO 2
+            $respBeneTmp = runPreparedQuery($conn, $c++,
+                    'SELECT id from tmp_db.benigeo where id=$1 and id_utente=$2 FOR UPDATE', [$My_POST['id'], $My_POST['id_utente']]);
+            if (!$respBeneTmp['ok'] || pg_num_rows($respBeneTmp['data']) <= 0) {
+                $res['msg'] = 'ID del bene in revisione non trovato. Forse altri revisori hanno approvato il bene.';
+                $error = true;
+            }
+            if (isset($My_POST['id_utente'])) {
+                // se viene fornito anche id_utente allora è parte della chiave per un bene in archivio temporaneo
+                // copio il bene temporaneo in archivio definitivo e cancello il bene temporaneo
+                // PASSO 3
+                $resp0 = replaceIntoBeniGeoTmp($conn, $c++, $My_POST['id'], $My_POST['ident'],
+                        $My_POST['descr'], $My_POST['mec'], $My_POST['meo'], $My_POST['bibl'],
+                        $My_POST['note'], $My_POST['topon'], $My_POST['comun'], $My_POST['geom'],
+                        $My_POST['id_utente'], $My_POST['status'], $My_POST['esist']);
+                $resp1 = upsertBeneTmpToBeniGeo($conn, $c++, $My_POST['id'], $My_POST['id_utente']);
+                // segno chi ha fatto cosa. PASSO 4
+                $resp2 = insertIntoManipolaBene($conn, $c++, $My_POST['id_utente'], $My_POST['id']);
+                $error = $error || !$resp0['ok'] || !$resp1['ok'] || !$resp2['ok'];
+                // PASSO 5
+                $resp3 = runPreparedQuery($conn, $c++,
+                        'DELETE FROM tmp_db.benigeo WHERE id=$1 AND id_utente=$2',
+                        [$My_POST['id'], $My_POST['id_utente']]);
+                // PASSO 6
+                $resp4 = runPreparedQuery($conn, $c++,
+                        'UPDATE FROM tmp_db.funzionigeo WHERE id_bene=$1 AND id_utente_bene=$2 SET id_utente_bene=NULL',
+                        [$My_POST['id'], $My_POST['id_utente']]);
+                $resp5 = runPreparedQuery($conn, $c++,
+                        'UPDATE FROM tmp_db.funzionigeo WHERE id_bener=$1 AND id_utente_bener=$2 SET id_utente_bener=NULL',
+                        [$My_POST['id'], $My_POST['id_utente']]);
+            }
         } else {
-            $res['msg'] = $transazione_fallita_msg;
+            $error = true;
+            http_response_code(401);
+            $res['msg'] = 'Operazione non permessa. Non sei un revisore';
         }
-    } else {
-        pg_query('ROLLBACK');
-        $failed_query = getFirstFailedQuery($queryArr);
-        if (!isset($res['msg']) && isset($failed_query)) { //magari ho già scritto io un messaggio d'errore
-            $res['msg'] = pg_result_error($failed_query['data']);
+
+        // per sicurezza controllo tutte le query
+        $queryArr = array($resp0, $resp1, $resp2, $resp3, $resp4, $resp5);
+        if (!$error && checkAllPreparedQuery($queryArr)) {
+            // se COMMIT è andato a buon fine
+            if (pg_query('COMMIT')) {
+                // se la transazione è andata a buon fine non la riprovo
+                $tCount = PHP_INT_MAX;
+                http_response_code(200);
+                logTxt($conn, "Valida bene", "ID utente: ${user['id']}, "
+                        . "ID bene: ${My_POST['id']}, ID utente bene: ${My_POST['id_utente']}");
+            } else {
+                $res['msg'] = $transazione_fallita_msg;
+            }
+        } else {
+            pg_query('ROLLBACK');
+            $failed_query = getFirstFailedQuery($queryArr);
+            if (!isset($res['msg']) && isset($failed_query)) { //magari ho già scritto io un messaggio d'errore
+                $res['msg'] = pg_result_error($failed_query['data']);
+            }
+            $msg = getOrDefault($res, 'msg', '');
+            logTxt($conn, "Valida bene fallito", "ID utente: ${user['id']}, "
+                    . "ID bene: ${My_POST['id']} - $msg");
         }
-        $msg = getOrDefault($res, 'msg', '');
-        logTxt($conn, "Valida bene fallito", "ID utente: ${user['id']}, "
-                . "ID bene: ${My_POST['id']} - $msg");
     }
 }
 
--- valida/funzioni/valida.php
+++ valida/funzioni/valida.php
@@ -29,108 +29,113 @@ if (!isset($user) && !$error) {
  * 6 cancellare la funzione nell'archivio temporaneo
  */
 if (isset($My_POST['id']) && !$error) {
-    // occorre proteggersi dalle possibili write skew risultanti 
+    // al massimo faccio il loop due volte nel caso la transazione fallisca
+    for ($tCount = 0; $tCount < 2; $tCount++) {
+            // occorre proteggersi dalle possibili write skew risultanti 
     // dalla modifica/creazione concorrente dello stesso bene da validare.
-    pg_query('BEGIN TRANSACTION ISOLATION LEVEL SERIALIZABLE') or die('Cant start transaction');
-    $respFunzioneTmp = $respFunzione = $resp0 = $resp1 = null;
-    $resp2 = $resp3 = $resp4 = $resp4 = $resp6 = $resp7 = null;
+        pg_query('BEGIN TRANSACTION ISOLATION LEVEL SERIALIZABLE') or die('Cant start transaction');
+        $respFunzioneTmp = $respFunzione = $resp0 = $resp1 = null;
+        $resp2 = $resp3 = $resp4 = $resp4 = $resp6 = $resp7 = null;
 
-    // PASSO 0 controllo benireferenziati. Cerco in archivio definitivo => funzione approvata richiede beni approvati
-    $b1_esiste = esisteBene($conn, $c++, $My_POST['id_bene'], null);
-    $b2_esiste = esisteBene($conn, $c++, $My_POST['id_bener'], null);
-    if ($b1_esiste && !isset($My_POST['id_bener'])) {
-        // ok bene 1 esiste e bene2=null
-    } else if ($b2_esiste && !isset($My_POST['id_bene'])) {
-        // ok bene 2 esiste e bene1=null
-    } else if ($b1_esiste && $b2_esiste) {
-        // ok entrambi i beni esistono
-    } else {
-        $b_inesistente = $b1_esiste ? $My_POST['id_bener'] : $My_POST['id_bene'];
-        http_response_code(422);
-        $error = true;
-        $res['msg'] = "Il bene $b_inesistente non esiste.";
-    }
-    if (!$error) {
-        // PASSO 1. controllo il ruolo.
-        if ($user['role'] == 'revisore') {
-            //PASSO 2
-            $respFunzioneTmp = runPreparedQuery($conn, $c++,
-                    'SELECT id from tmp_db.funzionigeo where id=$1 and id_utente=$2 FOR UPDATE',
-                    [$My_POST['id'], $My_POST['id_utente']]);
-            if (!$respFunzioneTmp['ok'] || pg_num_rows($respFunzioneTmp['data']) <= 0) {
-                $res['msg'] = 'ID della funzione in revisione non trovato. Forse altri revisori hanno approvato la funzione.';
-                $error = true;
-            }
-            // metto un lock sulla funzione in archivio definitivo se esiste
-            $respFunzione = runPreparedQuery($conn, $c++,
-                    'SELECT id from public.funzionigeo where id=$1 FOR UPDATE', [$My_POST['id']]);
-            // controllo sia andata a buon fine la query senza sovrascrivere $error
-            $error = $error || !$respFunzione['ok'];
-            if (!$error) {
-                if (isset($My_POST['id_utente'])) {
-                    // PASSO 3
-                    $resp0 = replaceIntoFunzioniGeoTmp($conn, $c++, $My_POST['id'], $My_POST['id_bene'],
-                            $My_POST['id_bener'], $My_POST['denominazione'], $My_POST['denominazioner'],
-                            $My_POST['data_ante'], $My_POST['data_poste'],
-                            $My_POST['tipodata'], $My_POST['funzione'], $My_POST['bibliografia'],
-                            $My_POST['note'], $My_POST['id_utente'], $My_POST['id_utente_bene'],
-                            $My_POST['id_utente_bener'], $My_POST['status']);
-                    $resp1 = upsertFunzioneTmpToFunzioniGeo($conn, $c++, $My_POST['id'], $My_POST['id_utente']);
-                    // ottengo l'id della funzione definitiva assegnata dal db
-                    $id_funzione_db = getIdFunzione($resp1);
-                    $resp2 = runPreparedQuery($conn, $c++,
-                            "UPDATE tmp_db.funzionigeo SET msg_validatore=NULL WHERE id=$1 AND id_utente=$2",
-                            [$id_funzione_db, $My_POST['id_utente']]);
-                    // cancello i ruoli precedenti in archivio definitivo per sostituirli
-                    $resp3 = runPreparedQuery($conn, $c++,
-                            "DELETE FROM public.funzionigeo_ruoli WHERE id_funzione=$1", [$id_funzione_db]);
-                    // inserisco i ruoli dei vari beni associati alla funzione in archivio definitivo
-                    // PASSO 4
-                    $resp4 = insertFunzioniGeoRuoli($conn, $c++, $id_funzione_db, $My_POST['id_utente'], $My_POST['ruolo'],
-                            $My_POST['ruolor'], false);
-                    // aggiunge N ruoli con N query preparate => devo incrementare l'id delle query preparate
-                    $maxLength = max(count($My_POST['ruolo']), count($My_POST['ruolor']));
-                    $c += $maxLength + 1;
-                    $error = $error || !$resp0['ok'] || !$resp1['ok'] || !$resp2['ok'] || !$resp3['ok'];
-                    //manipolafunzione serve se è validato il bene, registra chi ha modificato
-                    // PASSO 5
-                    $resp5 = insertIntoManipolaFunzione($conn, $c++, $My_POST['id_utente'], $id_funzione_db);
-                    // PASSO 6
-                    // cancello ruoli e funzione dal db temporaneo
-                    $resp6 = runPreparedQuery($conn, $c++,
-                            'DELETE FROM tmp_db.funzionigeo_ruoli WHERE id_funzione=$1 AND id_utente=$2',
-                            [$id_funzione_db, $My_POST['id_utente']]);
-                    $resp7 = runPreparedQuery($conn, $c++,
-                            'DELETE FROM tmp_db.funzionigeo WHERE id=$1 AND id_utente=$2',
-                            [$id_funzione_db, $My_POST['id_utente']]);
-                }
-            }
+        // PASSO 0 controllo benireferenziati. Cerco in archivio definitivo => funzione approvata richiede beni approvati
+        $b1_esiste = esisteBene($conn, $c++, $My_POST['id_bene'], null);
+        $b2_esiste = esisteBene($conn, $c++, $My_POST['id_bener'], null);
+        if ($b1_esiste && !isset($My_POST['id_bener'])) {
+            // ok bene 1 esiste e bene2=null
+        } else if ($b2_esiste && !isset($My_POST['id_bene'])) {
+            // ok bene 2 esiste e bene1=null
+        } else if ($b1_esiste && $b2_esiste) {
+            // ok entrambi i beni esistono
         } else {
+            $b_inesistente = $b1_esiste ? $My_POST['id_bener'] : $My_POST['id_bene'];
+            http_response_code(422);
             $error = true;
-            http_response_code(401);
-            $res['msg'] = 'Operazione non permessa. Non sei un revisore';
+            $res['msg'] = "Il bene $b_inesistente non esiste.";
         }
-    }
-    // per sicurezza controllo tutte le query
-    $queryArr = array($respFunzioneTmp, $respFunzione, $resp0, $resp1,
-        $resp2, $resp3, $resp4, $resp4, $resp6, $resp7);
-    if (!$error && checkAllPreparedQuery($queryArr)) {
-        // se COMMIT è andato a buon fine
-        if (pg_query('COMMIT')) {
-            http_response_code(200);
-            logTxt($conn, "Valida funzione", "ID utente: ${user['id']}, "
-                    . "ID funzione: ${My_POST['id']}, ID utente funzione: ${My_POST['id_utente']}");
+        if (!$error) {
+            // PASSO 1. controllo il ruolo.
+            if ($user['role'] == 'revisore') {
+                //PASSO 2
+                $respFunzioneTmp = runPreparedQuery($conn, $c++,
+                        'SELECT id from tmp_db.funzionigeo where id=$1 and id_utente=$2 FOR UPDATE',
+                        [$My_POST['id'], $My_POST['id_utente']]);
+                if (!$respFunzioneTmp['ok'] || pg_num_rows($respFunzioneTmp['data']) <= 0) {
+                    $res['msg'] = 'ID della funzione in revisione non trovato. Forse altri revisori hanno approvato la funzione.';
+                    $error = true;
+                }
+                // metto un lock sulla funzione in archivio definitivo se esiste
+                $respFunzione = runPreparedQuery($conn, $c++,
+                        'SELECT id from public.funzionigeo where id=$1 FOR UPDATE', [$My_POST['id']]);
+                // controllo sia andata a buon fine la query senza sovrascrivere $error
+                $error = $error || !$respFunzione['ok'];
+                if (!$error) {
+                    if (isset($My_POST['id_utente'])) {
+                        // PASSO 3
+                        $resp0 = replaceIntoFunzioniGeoTmp($conn, $c++, $My_POST['id'], $My_POST['id_bene'],
+                                $My_POST['id_bener'], $My_POST['denominazione'], $My_POST['denominazioner'],
+                                $My_POST['data_ante'], $My_POST['data_poste'],
+                                $My_POST['tipodata'], $My_POST['funzione'], $My_POST['bibliografia'],
+                                $My_POST['note'], $My_POST['id_utente'], $My_POST['id_utente_bene'],
+                                $My_POST['id_utente_bener'], $My_POST['status']);
+                        $resp1 = upsertFunzioneTmpToFunzioniGeo($conn, $c++, $My_POST['id'], $My_POST['id_utente']);
+                        // ottengo l'id della funzione definitiva assegnata dal db
+                        $id_funzione_db = getIdFunzione($resp1);
+                        $resp2 = runPreparedQuery($conn, $c++,
+                                "UPDATE tmp_db.funzionigeo SET msg_validatore=NULL WHERE id=$1 AND id_utente=$2",
+                                [$id_funzione_db, $My_POST['id_utente']]);
+                        // cancello i ruoli precedenti in archivio definitivo per sostituirli
+                        $resp3 = runPreparedQuery($conn, $c++,
+                                "DELETE FROM public.funzionigeo_ruoli WHERE id_funzione=$1", [$id_funzione_db]);
+                        // inserisco i ruoli dei vari beni associati alla funzione in archivio definitivo
+                        // PASSO 4
+                        $resp4 = insertFunzioniGeoRuoli($conn, $c++, $id_funzione_db, $My_POST['id_utente'], $My_POST['ruolo'],
+                                $My_POST['ruolor'], false);
+                        // aggiunge N ruoli con N query preparate => devo incrementare l'id delle query preparate
+                        $maxLength = max(count($My_POST['ruolo']), count($My_POST['ruolor']));
+                        $c += $maxLength + 1;
+                        $error = $error || !$resp0['ok'] || !$resp1['ok'] || !$resp2['ok'] || !$resp3['ok'];
+                        //manipolafunzione serve se è validato il bene, registra chi ha modificato
+                        // PASSO 5
+                        $resp5 = insertIntoManipolaFunzione($conn, $c++, $My_POST['id_utente'], $id_funzione_db);
+                        // PASSO 6
+                        // cancello ruoli e funzione dal db temporaneo
+                        $resp6 = runPreparedQuery($conn, $c++,
+                                'DELETE FROM tmp_db.funzionigeo_ruoli WHERE id_funzione=$1 AND id_utente=$2',
+                                [$id_funzione_db, $My_POST['id_utente']]);
+                        $resp7 = runPreparedQuery($conn, $c++,
+                                'DELETE FROM tmp_db.funzionigeo WHERE id=$1 AND id_utente=$2',
+                                [$id_funzione_db, $My_POST['id_utente']]);
+                    }
+                }
+            } else {
+                $error = true;
+                http_response_code(401);
+                $res['msg'] = 'Operazione non permessa. Non sei un revisore';
+            }
+        }
+        // per sicurezza controllo tutte le query
+        $queryArr = array($respFunzioneTmp, $respFunzione, $resp0, $resp1,
+            $resp2, $resp3, $resp4, $resp4, $resp6, $resp7);
+        if (!$error && checkAllPreparedQuery($queryArr)) {
+            // se COMMIT è andato a buon fine
+            if (pg_query('COMMIT')) {
+                // se la transazione è andata a buon fine non la riprovo
+                $tCount = PHP_INT_MAX;
+                http_response_code(200);
+                logTxt($conn, "Valida funzione", "ID utente: ${user['id']}, "
+                        . "ID funzione: ${My_POST['id']}, ID utente funzione: ${My_POST['id_utente']}");
+            } else {
+                $res['msg'] = $transazione_fallita_msg;
+            }
         } else {
-            $res['msg'] = $transazione_fallita_msg;
+            pg_query('ROLLBACK');
+            $failed_query = getFirstFailedQuery($queryArr);
+            if (!isset($res['msg']) && isset($failed_query)) //magari ho già scritto io un messaggio d'errore
+                $res['msg'] = pg_result_error($failed_query['data']);
+            $msg = getOrDefault($res, 'msg', '');
+            logTxt($conn, "Valida funzione fallita", "ID utente: ${user['id']}, "
+                    . "ID funzione: ${My_POST['id']} - $msg");
         }
-    } else {
-        pg_query('ROLLBACK');
-        $failed_query = getFirstFailedQuery($queryArr);
-        if (!isset($res['msg']) && isset($failed_query)) //magari ho già scritto io un messaggio d'errore
-            $res['msg'] = pg_result_error($failed_query['data']);
-        $msg = getOrDefault($res, 'msg', '');
-        logTxt($conn, "Valida funzione fallita", "ID utente: ${user['id']}, "
-                . "ID funzione: ${My_POST['id']} - $msg");
     }
 }
 echo json_encode($res);
