6d578cf216b8e54aaa2fe8fa494eb1006aa1f30d
mauricci <MaurizioRicci@users.noreply.github.com>
2020-03-24 15:19:42 +0100

    campo rinominato nel db

--- getDictionary.php
+++ getDictionary.php
@@ -1,29 +1,30 @@
 <?php
+
 include('connection.php');
 
 $res = array();
 http_response_code(500);
 
-if (isset($_GET['dict_name']) ) {
-	
-	$dict_name = $_GET['dict_name'];
-	$dict_name_esc = pg_escape_identifier($dict_name);
-	$result = pg_prepare($conn,'', "SELECT DISTINCT nome, row_number FROM vocabolari.$dict_name_esc ORDER BY row_number ASC");
-	if($result){
-		$result = pg_execute($conn,'', []);
-		if (!$result) {
-		  echo "An error occurred.\n";
-		  exit;
-		}
-		
-		while ($row = pg_fetch_row($result)) {
-			$temp = array(
-			  'id' => $row[0],
-			  'value' => $row[0]);
-		  array_push($res, $temp);
-		}
-		http_response_code(200);
-	}
+if (isset($_GET['dict_name'])) {
+
+    $dict_name = $_GET['dict_name'];
+    $dict_name_esc = pg_escape_identifier($dict_name);
+    $result = pg_prepare($conn, '', "SELECT DISTINCT nome, row_order FROM vocabolari.$dict_name_esc ORDER BY row_order ASC");
+    if ($result) {
+        $result = pg_execute($conn, '', []);
+        if (!$result) {
+            echo "An error occurred.\n";
+            exit;
+        }
+
+        while ($row = pg_fetch_row($result)) {
+            $temp = array(
+                'id' => $row[0],
+                'value' => $row[0]);
+            array_push($res, $temp);
+        }
+        http_response_code(200);
+    }
 }
 header('Content-type: application/json');
 echo json_encode($res);
